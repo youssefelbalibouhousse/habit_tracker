1a7216d0a17afe4e73542dbe6dbb4c245f8eb4ba
youss <youssef.elbali@gmail.com>
2025-06-22 06:22:26 +0200

    Initial commit

---
+++ src/test/java/org/youssef/habit_tracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youssef.habit_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
